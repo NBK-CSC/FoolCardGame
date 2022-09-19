3a152bdb5f00bcd9ae717d5a52aab2580d5264c8
NBK <1justforstuding1@gmail.com>
2022-09-19 19:29:16 +0300

    add table

---
+++ Assets/Scripts/Tables/Table.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Cards;
+using States;
+using UnityEngine;
+
+namespace Tables
+{
+    public class Table
+    {
+        private List<ICardData> _lowerCards;
+        private ICardData[] _upperCards;
+        private readonly Suit _trump;
+
+        public event Action<ICardData> OnCardLaid;
+        public event Action<ICardData, int> OnCardBeaten;
+        
+        public Table(Suit trump)
+        {
+            _trump = trump;
+            
+            _lowerCards = new List<ICardData>();
+            _upperCards = new ICardData[6];
+        }
+        
+        public bool TryTakeCard(ICardData lowerCard, StatusPlayer status)
+        {
+            if (status != StatusPlayer.Thrower || _lowerCards.Count == 6)
+                return false;
+            _lowerCards.Add(lowerCard);
+            OnCardLaid?.Invoke(lowerCard);
+            return true;
+        }
+
+        public bool TryTakeCard(ICardData lowerCard, ICardData upperCard, StatusPlayer status)
+        {
+            if (status != StatusPlayer.Defender || CheckPar(lowerCard, upperCard) == false)
+                return false;
+            var index = _lowerCards.IndexOf(lowerCard);
+            _upperCards[index] = upperCard;
+            OnCardBeaten?.Invoke(upperCard, index);
+            return true;
+        }
+        
+        private bool CheckPar(ICardData lowerCard, ICardData upperCard)
+        {
+            if (lowerCard.Suit == upperCard.Suit ) 
+                return lowerCard.Seniority < upperCard.Seniority;
+            return upperCard.Suit == _trump;
+        }
+    }
+}
---
+++ Assets/Scripts/Tables/TablePresenter.cs
@@ -0,0 +1,43 @@
+﻿using Cards;
+
+namespace Tables
+{
+    public class TablePresenter
+    {
+        private readonly Table _model;
+        private readonly PlayArea _view;
+
+        public TablePresenter(Table model, PlayArea view)
+        {
+            _model = model;
+            _view = view;
+        }
+
+        public void Enable()
+        {
+            _model.OnCardBeaten += AddCardInView;
+            _model.OnCardLaid += AddCardInView;
+        }
+
+        public void Disable()
+        {
+            _model.OnCardBeaten -= AddCardInView;
+            _model.OnCardLaid -= AddCardInView;
+        }
+
+        private void AddCardInView(ICardData data, int index)
+        {
+            _view.AddCard(data, index);
+        }
+        
+        private void AddCardInView(ICardData data)
+        {
+            _view.AddCard(data);
+        }
+        
+        private void RemoveCardInView(ICardData data)
+        {
+            _view.RemoveCard(data);
+        }
+    }
+}
