5c199152f5d97688483f78aeca164de249f8a4d7
NBK <1justforstuding1@gmail.com>
2023-06-03 22:53:52 +0300

    add windows

---
+++ Assets/Feature/Window/Scripts/Behaviours/Window.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace FoolCardGame.Windows.Behaviours
+{
+    [RequireComponent(typeof(Canvas))]
+    public class Window : MonoBehaviour
+    {
+        public void Open()
+        {
+            gameObject.SetActive(true);    
+        }
+        
+        public void Close()
+        {
+            gameObject.SetActive(false);    
+        }
+    }
+}
