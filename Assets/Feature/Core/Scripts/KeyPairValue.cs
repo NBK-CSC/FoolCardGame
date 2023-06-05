60cd39fcc8b10cecce62e877f6553f732716f756
NBK <1justforstuding1@gmail.com>
2023-06-05 15:00:44 +0300

    add serializable dictionary

---
+++ Assets/Feature/Core/Scripts/KeyPairValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace FoolCardGame.Core
+{
+    /// <summary>
+    /// Сущность ключ значения для серилизованного словаря
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    [Serializable]
+    public class KeyPairValue<K, V>
+    {
+        [SerializeField] private K _key;
+        [SerializeField] private V _value;
+        
+        public K Key => _key;
+
+        public V Value => _value;
+        
+        public KeyPairValue(K key, V value)
+        {
+            _key = key;
+            _value = value;
+        }
+    }
+}
